7547aa102f5090c67ebc6b0b7e590af0a5feb918
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-03 21:44:30 +0530

    modified

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,11 +1,6 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ca-central-1"
-}
-
 # Create API Gateway REST API
 resource "aws_api_gateway_rest_api" "website_dev" {
-  name        = "Website-Dev"
+  name        = "Website-Dev-AD"
   description = "Website-Dev API"
 }
 
