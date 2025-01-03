819030f31e1f88dd275018e1ea449b29ccab7c91
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-03 22:54:55 +0530

    modified

--- remote_state_backend.tf
+++ remote_state_backend.tf
@@ -1,62 +1,62 @@
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "terraform-state-bucket-inhouse-website-api-AD"
-# }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bucket-inhouse-website-api-AD"
+}
 
-# resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_lifecycle_configuration" "terraform_state_lifecycle" {
-#   bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_lifecycle_configuration" "terraform_state_lifecycle" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-#   rule {
-#     id     = "ExpireOldVersions"
-#     status = "Enabled"
+  rule {
+    id     = "ExpireOldVersions"
+    status = "Enabled"
 
-#     expiration {
-#       days = 30
-#     }
-#   }
-# }
+    expiration {
+      days = 30
+    }
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "terraform_state" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-state-locks-inhouse-website-api-AD"
-#   billing_mode = "PAY_PER_REQUEST"
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks-inhouse-website-api-AD"
+  billing_mode = "PAY_PER_REQUEST"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-#   hash_key = "LockID"
-# }
+  hash_key = "LockID"
+}
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-bucket-inhouse-website-api-AD"
-#     key            = "state/statefile.tfstate"
-#     region         = "ca-central-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-state-locks-inhouse-website-api-AD"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-inhouse-website-api-AD"
+    key            = "state/statefile.tfstate"
+    region         = "ca-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-locks-inhouse-website-api-AD"
+  }
+}
